17a1ccea8a7acc3ced2b4a463b66605d0c7639f4
Faus-MSI <faausc@gmail.com>
2025-05-29 11:14:41 -0300

    url parametrizada de bd

--- controllers/db.js
+++ controllers/db.js
@@ -1,11 +1,12 @@
 // controllers/db.js
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '251199',
-    database: 'erp_distri', 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
 
 
